8051490e8072a0064489d3f513620a52d48b697b
Sudheesan <surendransudheesan@gmail.com>
2022-08-04 21:33:09 -0500

    intigrated redux toolkit

---
+++ Alumni-Tracking-System-Client/src/components/modules/actions/studentActions.js
@@ -0,0 +1,10 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { getProducts } from "../../../services/productService";
+import { FETCH_ALL_STUDENTS } from "./actionTypes";
+
+const fetchAllStudents = createAsyncThunk(FETCH_ALL_STUDENTS, async () => {
+  const students = await getProducts();
+  return students;
+});
+
+export { fetchAllStudents };
