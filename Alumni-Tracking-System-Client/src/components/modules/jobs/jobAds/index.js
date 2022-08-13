2c69b75280cd1eacdc09f26084d9c796602352d1
Sudheesan <surendransudheesan@gmail.com>
2022-08-12 20:24:38 -0500

    updated changes
    updated changes

--- Alumni-Tracking-System-Client/src/components/modules/jobs/jobAds/index.js
+++ Alumni-Tracking-System-Client/src/components/modules/jobs/jobAds/index.js
@@ -11,8 +11,8 @@ import IconButton from "@mui/material/IconButton";
 import Select from "@mui/material/Select";
 import { Grid } from "@mui/material";
 import ResetTvSharpIcon from "@mui/icons-material/ResetTvSharp";
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 import SampleJob from "../jobCard";
 import JobApplyModal from "../jobAds/applyModal";
@@ -58,12 +58,11 @@ const JobAdList = () => {
 
     const filteredTagValues = value.map((tg) => tg.tag);
 
-    // if (filteredTagValues.length) {
-    //   filteredJobs = filteredJobs.filter((job) =>
-    //     job.tag.some((tag) => filteredTagValues.includes(tag))
-    //   );
-    // }
-
+    if (filteredTagValues.length) {
+      filteredJobs = filteredJobs.filter((job) =>
+        job.tags.some((tg) => filteredTagValues.includes(tg.tag))
+      );
+    }
     if (selectedState && selectedState !== "") {
       filteredJobs = filteredJobs.filter((job) => job.state === selectedState);
     }
