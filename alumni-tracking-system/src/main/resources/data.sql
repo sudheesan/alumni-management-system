7450434e8692f6eb15d5da067e1cb45d2b27f7c8
Sudheesan <surendransudheesan@gmail.com>
2022-08-13 19:14:05 -0500

    Update data.sql

--- alumni-tracking-system/src/main/resources/data.sql
+++ alumni-tracking-system/src/main/resources/data.sql
@@ -21,11 +21,11 @@ INSERT INTO public.tag (id, deleted, tag) VALUES (1005, false, 'Python');
 INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (1, 'Dover', 'Google', false, 'Java developers are needed.', 'Delaware', null, 3000);
 INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (2, 'Adams Morgan', 'Apple', false, 'IOS developers are needed.', 'District of Columbia', null, 3000);
 INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (3, 'Houston', 'Sysco LABS', false, 'We need React Js developers', 'Texas', null, 3000);
-INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (4, 'Alva', 'PayPal', false, 'We need java Javascript developers', 'Florida', null, 2);
-INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (5, 'Houston', 'Tesla', false, 'We need Python Devs', 'Texas', null, 2);
-INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (6, 'Dover', 'Meta', false, 'We need SpringBoot developers', 'Delaware', null, 2);
-INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (7, 'Watertown', 'Relisource', false, 'We need .Net Developers', 'New York', null, 2);
-INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (8, 'Avondale', 'Nasa', false, 'We need Java developers', 'Arizona', null, 2);
+INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (4, 'Alva', 'PayPal', false, 'We need java Javascript developers', 'Florida', null, 2000);
+INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (5, 'Houston', 'Tesla', false, 'We need Python Devs', 'Texas', null, 2000);
+INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (6, 'Dover', 'Meta', false, 'We need SpringBoot developers', 'Delaware', null, 2000);
+INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (7, 'Watertown', 'Relisource', false, 'We need .Net Developers', 'New York', null, 2000);
+INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (8, 'Avondale', 'Nasa', false, 'We need Java developers', 'Arizona', null, 2000);
 INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (9, 'Manchester', 'New York Times', false, 'We need Angular Developers', 'New York', null, 3000);
 INSERT INTO public.job (id, city, company_name, deleted, description, state, title, posted_by_id) VALUES (10, 'Anoka County', 'Marvel', false, 'We need JS devs', 'Minnesota', null, 3000);
 
